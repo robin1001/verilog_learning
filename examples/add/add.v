a9964192bd4a44b53a3ee115b8110aa2feb248e9
Binbin Zhang <binbzha@qq.com>
2024-04-02 17:16:19 +0800

    add carry flag

--- examples/add/add.v
+++ examples/add/add.v
@@ -1,9 +1,10 @@
 module sum (
     input [7:0] x,
     input [7:0] y,
-    output [7:0] z
+    output [7:0] z,
+    output cout
 );
 
-    assign z = x + y;
+    assign {cout, z} = x + y;
 
 endmodule
--- examples/add/add_tb.v
+++ examples/add/add_tb.v
@@ -3,19 +3,21 @@ module sum_tb();
     reg [7:0] x;
     reg [7:0] y;
     wire [7:0] z;
+    wire cout;
 
     sum s(
         .x(x),
         .y(y),
-        .z(z)
+        .z(z),
+        .cout(cout)
     );
 
     initial begin
-      x = 8'b0000_0000;
+      x = 8'b1111_1110;
       y = 8'b0000_0001;
 
       #5;
-      $display("x = %b, y = %b, z = %b", x, y, z);
+      $display("x = %b, y = %b, z = %b, cout = %b", x, y, z, cout);
 
       $finish;
     end
